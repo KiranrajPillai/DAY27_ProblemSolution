a0b9740945a25cf7b87f6ef2cdb8730c4ee73b94
KiranrajPillai <kiranrajpillai1997@gmail.com>
2021-08-16 23:10:28 +0530

    Hello

---
+++ src/com/AddressBook/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.AddressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBookManager makeBook = new AddressBookManager();
+        makeBook.takeOption();
+        boolean condition = true;
+        while (condition) {
+            boolean monitorUser = makeBook.takeOption();
+            if (monitorUser)
+                break;
+        }
+    }
+}
